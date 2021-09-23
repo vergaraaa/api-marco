cdff9949037837b59d8d0e7ba2282620b4c493c9
vergaraaa <67242290+vergaraaa@users.noreply.github.com>
2021-09-23 12:06:44 -0500

    Changes in User model

--- src/models/User.js
+++ src/models/User.js
@@ -3,14 +3,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const User = new Schema ({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
+    password: { type: String, required: true }
 }, {
     timestamps: true
 });
